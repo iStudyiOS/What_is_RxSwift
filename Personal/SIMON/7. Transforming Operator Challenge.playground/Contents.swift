d90aaf1f9adcc5ebe403d41e90422943e51459b4
sanghunlee <simoniful0@gmail.com>
2022-04-15 02:45:11 +0900

    Add: Transforming Operator Challenge 정리

--- Personal/SIMON/7. Transforming Operator Challenge.playground/Contents.swift
+++ Personal/SIMON/7. Transforming Operator Challenge.playground/Contents.swift
@@ -2,89 +2,110 @@ import Foundation
 import RxSwift
 
 example(of: "Challenge 1") {
-// 1. skipWhile을 사용: 전화번호는 0으로 시작할 수 없습니다.
-// 2. filter를 사용: 각각의 전화번호는 한자리의 숫자 (10보다 작은 숫자)여야 합니다.
-// 3. take와 toArray를 사용하여, 10개의 숫자만 받도록 하세요. (미국 전화번호처럼)
-    
   let disposeBag = DisposeBag()
-  
+
   let contacts = [
     "603-555-1212": "Florent",
     "212-555-1212": "Shai",
     "408-555-1212": "Marin",
     "617-555-1212": "Scott"
   ]
-  
-  func phoneNumber(from inputs: [Int]) -> String {
-    var phone = inputs.map(String.init).joined()
-    
+
+  let convert: (String) -> Int? = { value in
+    if let number = Int(value),
+       number < 10 {
+      return number
+    }
+
+    let keyMap: [String: Int] = [
+      "abc": 2, "def": 3, "ghi": 4,
+      "jkl": 5, "mno": 6, "pqrs": 7,
+      "tuv": 8, "wxyz": 9
+    ]
+
+    let converted = keyMap
+      .filter { $0.key.contains(value.lowercased()) }
+      .map(\.value)
+      .first
+
+    return converted
+  }
+
+  let format: ([Int]) -> String = {
+    var phone = $0.map(String.init).joined()
+
     phone.insert("-", at: phone.index(
       phone.startIndex,
       offsetBy: 3)
     )
-    
+
     phone.insert("-", at: phone.index(
       phone.startIndex,
       offsetBy: 7)
     )
-    
+
     return phone
   }
-  
-  let input = PublishSubject<Int>()
-  
-  // Add your code here
-  input.skipWhile { number in number == 0 }
-    .filter { number in number < 10 }
-    .take(10)
-    .toArray()
-    .subscribe {
-        let phone = phoneNumber(from: $0)
-        if let contact = contacts[phone] {
-          print("Dialing \(contact) (\(phone))...")
-          // Dialing Shai (212-555-1212)...
-        } else {
-          print("Contact not found")
-        }
-    } onError: { error in
-        print("Error", error.localizedDescription)
-    }
-    .disposed(by: disposeBag)
-  
-  // skipWhile 조건에 맞지 않아 스킵
-  input.onNext(0)
-  // filter 조건에 맞지 않아 스킵
-  input.onNext(603)
-  
-  input.onNext(2)
-  input.onNext(1)
-  
-  // Confirm that 7 results in "Contact not found",
-  // and then change to 2 and confirm that Shai is found
-  // input.onNext(7) -> input.onNext(2)
-  input.onNext(2)
-  
-  "5551212".forEach {
-    if let number = (Int("\($0)")) {
-      input.onNext(number)
+
+  let dial: (String) -> String = {
+    if let contact = contacts[$0] {
+      return "Dialing \(contact) (\($0))..."
+    } else {
+      return "Contact not found"
     }
   }
-  
-  input.onNext(9)
+
+  let input = PublishSubject<String>()
+
+  // Add your code here
+  input
+    // 한 자릿수 이상의 값 필터, 문자열로 주어질 경우 Int 변환
+    .map(convert)
+    // nil 값 지우기 ""
+    .unwrap()
+    // 0이 아닌 요소가 나올 때까지 skip
+    .skipWhile { $0 == 0 }
+    // 초기 방출 next 10개 취하고 나머지는 버림
+    .take(10)
+    // 배열 전환
+    .toArray()
+    // - 포함 글자 만들기
+    .map(format)
+    // 연락처에서 번호에 맞는 이름 값 전환
+    .map(dial)
+    .subscribe(onSuccess: {
+      print($0)
+    })
+    .disposed(by: disposeBag)
+
+  input.onNext("")
+  input.onNext("0")
+  input.onNext("408")
+
+  input.onNext("6")
+  input.onNext("")
+  input.onNext("0")
+  input.onNext("3")
+
+  "JKL1A1B".forEach {
+    input.onNext("\($0)")
+  }
+
+  input.onNext("9")
 }
 
 /// Copyright (c) 2020 Razeware LLC
-/// 
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -92,7 +113,7 @@ example(of: "Challenge 1") {
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
